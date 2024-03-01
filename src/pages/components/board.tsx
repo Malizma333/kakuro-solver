6246a01d35b0f492d26b73694873fafcb7f6e08f
tkbessler <tkbessler@gmail.com>
2024-03-01 14:06:47 -0600

    basic layout

--- src/pages/components/board.tsx
+++ src/pages/components/board.tsx
@@ -1,6 +1,6 @@
 export default function Board() {
   return (
-    <div className="border">
+    <div className="border w-full flex-1 m-5 flex justify-center items-center">
       Board Here
     </div>
   )
--- src/pages/components/toolbar.tsx
+++ src/pages/components/toolbar.tsx
@@ -1,6 +1,6 @@
 export default function Toolbar() {
   return (
-    <div className="border">
+    <div className="border rounded-lg w-full h-14 flex justify-center items-center">
       Toolbar Here
     </div>
   )
