3b7970dac8a408119a6b8d25cd05746cd87fc46e
tkbessler <tkbessler@gmail.com>
2024-03-08 19:38:18 -0600

    forward and back buttons

--- src/pages/components/toolbar.tsx
+++ src/pages/components/toolbar.tsx
@@ -1,36 +1,58 @@
 import { Board } from "@/lib/board"
 
-export default function ToolbarComponent({board, setWidth, setHeight}:{board: Board, setWidth: Function, setHeight: Function}) {
+export default function ToolbarComponent(
+  {props}:{props: {board: Board, setWidth: Function, setHeight: Function, toolPage: number, setToolPage: Function}}
+) {
   return (
-    <div className="rounded-lg w-full h-14 flex justify-center items-center">
-      <label className="m-3" htmlFor="width">Width</label>
-      <input className="border rounded-lg bg-black w-10"
-        id="width"
-        type="number"
-        min={Board.minDimension}
-        max={Board.maxDimension}
-        placeholder={Board.defaultDimension.toString()}
-        value={board.width}
-        onChange={(e) => {
-          const newWidth = Math.min(Board.maxDimension, Math.max(Board.minDimension, parseInt(e.target.value)));
-          board.reset(newWidth, undefined);
-          setWidth(newWidth);
-        }}
-      />
-      <label className="m-3" htmlFor="height">Height</label>
-      <input className="border rounded-lg bg-black w-10"
-        id="height"
-        type="number"
-        min={Board.minDimension}
-        max={Board.maxDimension}
-        placeholder={Board.defaultDimension.toString()}
-        value={board.height}
-        onChange={(e) => {
-          const newHeight = Math.min(Board.maxDimension, Math.max(Board.minDimension, parseInt(e.target.value)));
-          board.reset(undefined, newHeight);
-          setHeight(newHeight);
-        }}
-      />
+    <div className="relative w-1/2 h-14 flex justify-center items-center">
+      <button
+        className="absolute left-0 border rounded-lg bg-black w-10 disabled:opacity-0"
+        disabled={props.toolPage === 0}
+        onClick={() => props.setToolPage(props.toolPage - 1)}
+      >
+        {"<"}
+      </button>
+
+      {props.toolPage === 0 &&
+        <div>
+        <label className="mr-3 ml-6" htmlFor="width">Width</label>
+        <input className="border rounded-lg bg-black w-10"
+          id="width"
+          type="number"
+          min={Board.minDimension}
+          max={Board.maxDimension}
+          placeholder={Board.defaultDimension.toString()}
+          value={props.board.width}
+          onChange={(e) => {
+            const newWidth = Math.min(Board.maxDimension, Math.max(Board.minDimension, parseInt(e.target.value)));
+            props.board.reset(newWidth, undefined);
+            props.setWidth(newWidth);
+          }}
+        />
+        <label className="mr-3 ml-6" htmlFor="height">Height</label>
+        <input className="border rounded-lg bg-black w-10"
+          id="height"
+          type="number"
+          min={Board.minDimension}
+          max={Board.maxDimension}
+          placeholder={Board.defaultDimension.toString()}
+          value={props.board.height}
+          onChange={(e) => {
+            const newHeight = Math.min(Board.maxDimension, Math.max(Board.minDimension, parseInt(e.target.value)));
+            props.board.reset(undefined, newHeight);
+            props.setHeight(newHeight);
+          }}
+        />
+        </div>
+      }
+
+      <button
+        className="absolute right-0 border rounded-lg bg-black w-10 disabled:opacity-0"
+        disabled={props.toolPage === 2}
+        onClick={() => props.setToolPage(props.toolPage + 1)}
+      >
+        {">"}
+      </button>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,12 +10,15 @@ const board = new Board();
 export default function Home() {
   const [width, setWidth] = useState(board.width);
   const [height, setHeight] = useState(board.height);
+  const [toolPage, setToolPage] = useState(0);
+
+  const toolbarProps = {board, setWidth, setHeight, toolPage, setToolPage};
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <ToolbarComponent board={board} setWidth={setWidth} setHeight={setHeight}></ToolbarComponent>
+      <ToolbarComponent props={toolbarProps}></ToolbarComponent>
       <BoardComponent board={board}></BoardComponent>
     </main>
   );
