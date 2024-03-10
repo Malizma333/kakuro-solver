58e8b17b203389f11661a30e12a381ad5da8512d
tkbessler <tkbessler@gmail.com>
2024-03-10 17:16:21 -0500

    text inputs done

--- src/pages/components/board.tsx
+++ src/pages/components/board.tsx
@@ -1,5 +1,90 @@
 import type { BoardType } from "@/lib/board"
 
+const cellSize = 'w-12 h-12'
+const halfSize = 'w-6 h-6'
+
+function SizeBoard(board: BoardType) {
+  return [...Array(board.width)].map((_,i) =>
+    <div key={i}>
+      {[...Array(board.height)].map((_,j) =>
+        <div className={`border ${cellSize} bg-black`} key={j}/>
+      )}
+    </div>
+  )
+}
+
+function ColorBoard(board: BoardType, setBoard: Function, swatch: 0 | 1) {
+  return [...Array(board.width)].map((_,i) =>
+    <div key={i}>
+      {[...Array(board.height)].map((_,j) => {
+        return <div
+          className={`border ${cellSize} cursor-pointer bg-${
+            board.state[i][j].type === 1 ? 'white' : 'black'
+          }`}
+          key={j}
+          onClick={() => {
+            const boardState = [...board.state];
+            boardState[i][j].type = swatch;
+            setBoard({...board, state: boardState});
+          }}
+        />
+      }
+      )}
+    </div>
+  )
+}
+
+function Hint(i: number, j: number, board: BoardType, setBoard: Function) {
+  return <div>
+    <input
+      className={`${halfSize} bg-transparent text-center float-right`}
+      type='number'
+      min={3}
+      max={45}
+      value={board.state[i][j].data[0]}
+      onChange={(e) => {
+        const boardState = {...board.state};
+        boardState[i][j].data[0] = Math.max(3, Math.min(45, parseInt(e.target.value)));
+        setBoard({...board, state: boardState} as BoardType)
+      }}
+    />
+    <input
+      className={`${halfSize} bg-transparent text-center`}
+      type='number'
+      min={3}
+      max={45}
+      value={board.state[i][j].data[1]}
+      onChange={(e) => {
+        const boardState = {...board.state};
+        boardState[i][j].data[1] = Math.max(3, Math.min(45, parseInt(e.target.value)));
+        setBoard({...board, state: boardState} as BoardType)
+      }}
+    />
+  </div>
+}
+
+function TextBoard(board: BoardType, setBoard: Function) {
+  return [...Array(board.width)].map((_,i) =>
+    <div key={i}>
+      {[...Array(board.height)].map((_,j) => {
+        switch(board.state[i][j].type) {
+          case 0:
+            return <div className={`border ${cellSize} bg-black`} key={j}/>
+          case 1:
+            return <div className={`border ${cellSize} bg-white`} key={j}/>
+          case 2:
+            return <div className={`border ${cellSize} bg-white bg-diagonal text-black`} key={j}>
+              {Hint(i, j, board, setBoard)}
+            </div>
+          default:
+            return null
+        }
+      }
+      )}
+    </div>
+  )
+}
+
 export default function BoardComponent(
   {props}:{props: {
     board: BoardType, setBoard: Function,
@@ -9,33 +94,9 @@ export default function BoardComponent(
 ) {
   return (
     <div className="w-full flex-1 m-5 flex justify-center items-center">
-      {props.toolPage === 0 &&
-      [...Array(props.board.width)].map((_,i) =>
-        <div key={i}>
-          {[...Array(props.board.height)].map((_,j) =>
-            <div className="border w-10 h-10 bg-black" key={j}/>
-          )}
-        </div>
-      )}
-      {props.toolPage === 1 &&
-      [...Array(props.board.width)].map((_,i) =>
-        <div key={i}>
-          {[...Array(props.board.height)].map((_,j) => {
-            return <div
-              className={`border w-10 h-10 cursor-pointer bg-${
-                props.board.state[i][j].type === 0 ? 'black' : 'white'
-              }`}
-              key={j}
-              onClick={() => {
-                const boardState = [...props.board.state];
-                boardState[i][j].type = props.swatch;
-                props.setBoard({...props.board, state: boardState});
-              }}
-            />
-          }
-          )}
-        </div>
-      )}
+      {props.toolPage === 0 && SizeBoard(props.board)}
+      {props.toolPage === 1 && ColorBoard(props.board, props.setBoard, props.swatch)}
+      {props.toolPage === 2 && TextBoard(props.board, props.setBoard)}
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "diagonal": `linear-gradient(to top right,
+          rgba(0,0,0,0) 0%,
+          rgba(0,0,0,0) calc(50% - 0.8px),
+          rgba(0,0,0,1) 50%,
+          rgba(0,0,0,0) calc(50% + 0.8px),
+          rgba(0,0,0,0) 100%)`
       },
     },
   },
